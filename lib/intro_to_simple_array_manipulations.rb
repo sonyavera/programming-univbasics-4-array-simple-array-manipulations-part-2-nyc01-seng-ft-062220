5eb52b2cf3c5d11bc92ab85e12251686f5560db4
Sonya Gould <sonyavgould@gmail.com>
2020-06-06 16:20:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,24 @@ def using_concat(first_array, second_array)
   first_array.concat(second_array)
 end
 
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
